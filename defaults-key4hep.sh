e614dd45d9a336f7fa928d6bf309ecb69b24d55c
Predrag Buncic <pbuncic@yahoo.com>
2026-06-25 13:25:57 +0200

    Adding key4hep

--- defaults-key4hep.sh
+++ defaults-key4hep.sh
@@ -1,16 +1,7 @@
 package: defaults-key4hep
 version: v1
 env:
-  CFLAGS: -fPIC -O2
-  CMAKE_BUILD_TYPE: RELWITHDEBINFO
-  CXXFLAGS: -fPIC -O2 -std=c++20
-  CXXSTD: '20'
-  ENABLE_VMC: 'ON'
-  GEANT4_BUILD_MULTITHREADED: 'OFF'
-  MACOSX_DEPLOYMENT_TARGET: '14.0'
-  ENABLE_IPO: 'OFF'
+
+overrides:
   
 ---
-# This file is included in any build recipe and it's only used to set
-# environment variables. Which file to actually include can be defined by the
-# "--defaults" option of alibuild.
--- defaults-release.sh
+++ defaults-release.sh
@@ -4,4 +4,20 @@ env:
   CXXFLAGS: "-fPIC -g -O2 -std=c++11"
   CFLAGS: "-fPIC -g -O2"
   CMAKE_BUILD_TYPE: "RELWITHDEBINFO"
+  MACOSX_DEPLOYMENT_TARGET: '14.0'
+  ENABLE_IPO: 'OFF'
+
+system:
+  sandbox_network: "off"
+  build_oversubscribe: 1.25
+  
+variables:
+  lcgversion: main
+
+requires:
+  lcg.bits
+
+overrides:
+  lcg.bits:
+    tag: %(lcgversion)s
 ---
